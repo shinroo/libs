7708dd84b1cf06e5ca16775eef370a8c34a10c64
Robert Focke <robert_focke@yahoo.com>
2018-11-02 21:57:35 +0200

    Update python2_utf_compatibility/utf8_compatibility.py

---
+++ python2_utf_compatibility/utf8_compatibility.py
@@ -0,0 +1,11 @@
+######################################################
+# UTF-8 COMPATIBILITY CODE ###########################
+######################################################
+
+import sys
+import codecs
+
+sys.stdout = codecs.getwriter('utf8')(sys.stdout)
+sys.stderr = codecs.getwriter('utf8')(sys.stderr)
+
+######################################################
